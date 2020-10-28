b625ce96ded44684a4dd461bdf9dd075fb1c1083
Nikolai <nikolaj.a.kondakov@gmail.com>
2020-10-28 13:03:13 +0600

    * Голосовые сообщения удаляются в указанном групповом чате * Пользователю, отправившему голосовое сообщение, присылывается уведомление, если это сделано в чате.   В личных сообщениях бот скажет, что не умеет слушать * Стикеры отключены в указанном групповом чате * Изменена информация «О проекте» * Слово «Команда» заменено на «Разработчики» * Класс «team_info» заменен на «developers_info»

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-api_id = 1856266
-api_hash = '1b57b54abd8214fe345ba814e51aa482'
-token = '1368849966:AAG5XVgj6F_5V_bynj6hP3s7AtoqV2Ork3o'
+#api_id = 1856266
+#api_hash = '1b57b54abd8214fe345ba814e51aa482'
+#token = '1368849966:AAG5XVgj6F_5V_bynj6hP3s7AtoqV2Ork3o'
+
+token = '1020063385:AAG5dWsOWwd6UgHQZnkh9FRl8oI3ozhnSx0'
+
+group_id = -1001281269402
--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def start(message):
 
 
 # Команда /team_info
-@bot.message_handler(commands=['team_info'])
-def team_info(message):
+@bot.message_handler(commands=['developers_info'])
+def developers_info(message):
     for name in team:
         start_message = name + ' - ' + team[name]  # выводим словарь
         bot.send_message(message.chat.id, start_message)
@@ -31,9 +31,9 @@ def team_info(message):
 @bot.message_handler(commands=['about'])
 def about(message):
     # Ниже выводим ключи из словаря team
-    about_message = "<b>Я — учебный проект 3 ФКН-щиков</b>\nИх зовут — " + ', '.join(
-        team.keys()) + '\nХотя пока тут балуется только последний из этих...'
-    bot.send_message(message.chat.id, about_message, parse_mode='html')
+    about_message = "*Я — учебный проект 3 ФКН-щиков*\nИх зовут — " + ', '.join(
+        team.keys()) + '\n\n[Проект на GitHub](https://github.com/i9d/superbottelegramskoleyinastey)'
+    bot.send_message(message.chat.id, about_message, parse_mode='markdown')
 
 
 # Команда /help
@@ -43,8 +43,16 @@ def help(message):
     bot.send_message(message.chat.id, help_message)
 
 
-# Обработка запросов (документов, фото, голосовых сообщений и текстовых сообщений)
-@bot.message_handler(content_types=['text', 'voice', 'document', 'photo'])
+# Модерация голосовых сообщений
+@bot.message_handler(content_types=['voice'])
+def get_voice(message):
+    print('Пришло голосовое сообщение от', message.from_user.username)
+    if (message.chat.id == config.group_id): delete_voise_message(message)
+    else: bot.send_message(message.chat.id, 'Я не умею слушать, прости')
+
+
+# Обработка запросов (документов, фото и текстовых сообщений)
+@bot.message_handler(content_types=['text'])
 def get_text(message):
     # обработка в консоль
     print('Пришло сообщение от', message.from_user.username + ':')
@@ -52,18 +60,25 @@ def get_text(message):
 
     if message.text == 'О проекте':
         about(message)
-    elif message.text == 'Команда':
-        team_info(message)
-    else:
-        bot.send_message(message.from_user.id, "Я пока не знаю что с этим делать. Попробуй написать /help")
+    elif message.text == 'Разработчики':
+        developers_info(message)
+    #else:
+    #    bot.send_message(message.from_user.id, "Я пока не знаю что с этим делать. Попробуй написать /help")
 
-
-# Когда бот получает стикер, будет отправлять случайные из папки stickers
+# Когда бот получает стикер, будет отправлять случайные из stickers_lib.py
 @bot.message_handler(content_types=['sticker'])
 def get_sticker(message):
     print('Получен стикер от', message.from_user.username)  # обработка в консоль
-    bot.send_sticker(message.chat.id, random.choice(stickers_lib.stickers_id))
+    if (message.chat.id != config.group_id):
+        bot.send_sticker(message.chat.id, random.choice(stickers_lib.stickers_id))
 
+# Удаление голосовых сообщений с предупреждением отправителя
+def delete_voise_message(message):
+    warming_message = '@' + str(message.from_user.username) + ', голосовые сообщения запрещены. Не надо их сюда отправлять'
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_message(message.chat.id, warming_message)
+    print('Начинаю удалять сообщение')
+    bot.send_message(message.chat.id, 'Я это пока просто удалю, а потом уже реализую бан.')
 
 # Бот постоянно ждёт для себя сообщения
 bot.polling(none_stop=True)
