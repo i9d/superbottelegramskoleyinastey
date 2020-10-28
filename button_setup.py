aae3030140885c66e4ed478304cf5151e0359312
Nikolai <nikolaj.a.kondakov@gmail.com>
2020-10-28 13:10:33 +0600

    * Голосовые сообщения удаляются в указанном групповом чате * Пользователю, отправившему голосовое сообщение, присылывается уведомление, если это сделано в чате.   В личных сообщениях бот скажет, что не умеет слушать * Стикеры отключены в указанном групповом чате * Изменена информация «О проекте» * Слово «Команда» заменено на «Разработчики» * Класс «team_info» заменен на «developers_info»

--- button_setup.py
+++ button_setup.py
@@ -3,5 +3,5 @@ from telebot import types
 # создание кнопок
 button = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button_info = types.KeyboardButton(text='О проекте')
-button_team_info = types.KeyboardButton(text='Команда')
+button_team_info = types.KeyboardButton(text='Разработчики')
 button.add(button_info, button_team_info)
